c5c9a966d0e420013f945a5aba43a023c8957233
Kirill-Marushko <k.v.marushko2001@gmail.com>
2025-01-27 14:25:52 +0100

    save_03

---
+++ save_03.py
@@ -0,0 +1,18 @@
+#Напишіть програму для знаходження всіх простих чисел в заданому діапазоні
+
+start = int(input("Введіть звідки починаємо: "))
+end = int(input("Введіть де закінчуємо: "))
+
+correct = []
+prime = True
+
+for i in range(start, end):
+    for i2 in range(2, i):
+        if i % i2 == 0:
+            prime = False
+            break
+    if prime is True:
+        correct.append(i)
+    prime = True
+print(correct)
+
