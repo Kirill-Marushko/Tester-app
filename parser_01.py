890a29693f619a905bd9387ccfbaab8350259047
Kirill-Marushko <k.v.marushko2001@gmail.com>
2025-02-03 13:30:24 +0100

    parser_01

--- parser_01.py
+++ parser_01.py
@@ -1,2 +1,23 @@
 # зробити словник, де ключом буде автор, а значенням вислів, якщо у автора не один вислів,
 # то переробити значення на список, та додати у цей список ще один вислів
+
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("https://quotes.toscrape.com/")
+soup = BeautifulSoup(r.content, "html.parser")
+
+lst1 = []
+for data in soup.select(".text"):
+    y = data.text
+    lst1.append(y)
+
+lst2 = []
+for data in soup.select(".author"):
+    s = data.text
+    lst2.append(s)
+
+x = {}
+for index, i in enumerate(lst2):
+    lst2[i] = lst1[index]
+print(lst1, lst2)
